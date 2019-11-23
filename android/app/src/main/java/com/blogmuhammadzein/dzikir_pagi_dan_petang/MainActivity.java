858e2dbcf8230d9c12396e4403d877eb23906c6d
Muhammad Zein <muhammadzein0321@gmail.com>
2019-11-23 13:42:13 +0700

    Initial commit

---
+++ android/app/src/main/java/com/blogmuhammadzein/dzikir_pagi_dan_petang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.blogmuhammadzein.dzikir_pagi_dan_petang;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
